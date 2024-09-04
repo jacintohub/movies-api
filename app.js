446bbb9e533b5544b208c8a5fc83b8daa8288426
jacintohub <mandekelela@gmail.com>
2024-09-04 06:40:41 -0300

    Atualizando backend com conexão ao MongoDB Atlas e melhorias

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
 // Importar as rotas de filmes
 const filmesRoutes = require("./routes/filmes");
+
+// Carregar variáveis de ambiente do arquivo .env
+require("dotenv").config();
 
 // Configurar o servidor express
 const app = express();
 
 // Habilitar CORS para todas as rotas
-app.use(cors()); // Adiciona o middleware CORS aqui
+app.use(cors());
 
-// Middleware para processar JSON
-app.use(bodyParser.json());
+// Middleware para processar JSON (não é mais necessário usar body-parser)
+app.use(express.json());
 
-// Conectar ao MongoDB
+// Conectar ao MongoDB usando a variável de ambiente
 mongoose
-  .connect("mongodb://localhost/filmesDB", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Conectado ao MongoDB"))
+  .then(() => console.log("Conectado ao MongoDB Atlas"))
   .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
 // Usar as rotas de filmes
 app.use("/api/filmes", filmesRoutes);
 
-// Definir porta do servidor
+// Definir porta do servidor com fallback para 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
